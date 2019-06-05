7464681d779e1caa7c39a0d88a174a26a49f5590
Ramon Brand <ramon2009@gmail.com>
2019-06-05 14:39:31 +0300

    (master): Extract clearscreen

--- src/index.ts
+++ src/index.ts
@@ -19,22 +19,8 @@ import GameState from './GameState'
 //////////////////////////////////////////////
 
 ////////////////////////////////////
-// Game Intro and Item Types
+// Item Types
 ////////////////////////////////////
-
-console.log("\nYou wake up. You're laying on the floor of a kid's bedroom.")
-console.log('You have no memory of any previous events.')
-console.log('\nYou find a note on the ground with some instructions written on it. The note says:')
-console.log('\n\tWelcome to AdventureGame(TM) by Sandels Entertainment!')
-console.log(
-  "\tYou can look around by typing 'look'. You can pick up and drop items by typing 'pick' or 'drop' and the item's name.",
-)
-console.log(
-  "\tYou can move to different rooms by typing 'move' and the room's name. You can attack by typing 'attack'.",
-)
-console.log("\tYou can use consumable items by typing 'eat' or 'drink' and the item's name.")
-console.log("\tYou can view this note again by typing 'help' at any time!")
-console.log('\n\tEnjoy the game!\n')
 
 export const TYPE_WEAPON = 0
 export const TYPE_SHIELD = 1
@@ -297,11 +283,31 @@ function printUI() {
   if (gameState.playerHealth > 0 || gameState.godmode) {
     console.log(`Your health: ${gameState.playerHealth}HP`)
   } else {
-    playerDeath()
+    gameEnd()
   }
 }
 
-function playerDeath() {
+function clearScreen() {
+  process.stdout.write('\u001b[2J\u001b[0;0H')
+}
+
+function gameStart() {
+  console.log("\nYou wake up. You're laying on the floor of a kid's bedroom.")
+  console.log('You have no memory of any previous events.')
+  console.log('\nYou find a note on the ground with some instructions written on it. The note says:')
+  console.log('\n\tWelcome to AdventureGame(TM) by Sandels Entertainment!')
+  console.log(
+    "\tYou can look around by typing 'look'. You can pick up and drop items by typing 'pick' or 'drop' and the item's name.",
+  )
+  console.log(
+    "\tYou can move to different rooms by typing 'move' and the room's name. You can attack by typing 'attack'.",
+  )
+  console.log("\tYou can use consumable items by typing 'eat' or 'drink' and the item's name.")
+  console.log("\tYou can view this note again by typing 'help' at any time!")
+  console.log('\n\tEnjoy the game!\n')
+}
+
+function gameEnd() {
   console.log('\x1b[31m%s\x1b[0m', `\nYour health is ${gameState.playerHealth}HP.`)
   console.log('\x1b[31m%s\x1b[0m', `\nOh dear, you're dead!`)
   console.log('\x1b[31m%s\x1b[0m', `Better luck next time!`)
@@ -364,13 +370,20 @@ const rl = readline.createInterface({
   prompt: '\n--> ',
 })
 
+////////////////////////////////////
+// Gameplay
+////////////////////////////////////
+
+clearScreen()
+gameStart()
+
 rl.prompt()
 rl.on('line', line => {
   line = line.trim()
   const words = line.split(' ')
   const args = words.slice(1)
 
-  process.stdout.write('\u001b[2J\u001b[0;0H') // Clear screen
+  clearScreen()
 
   perform(words[0], args)
 
