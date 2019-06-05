9d447b8505fee68737d21e5ef8d019cf4d41aaff
Ramon Brand <ramon2009@gmail.com>
2019-06-05 12:38:32 +0300

    (master): Add content loader

---
+++ src/ContentLoader.ts
@@ -0,0 +1,22 @@
+import { existsSync, readFileSync } from "fs"
+
+const CONTENT_ROOT = 'content/'
+
+export function loadFile<T>(filename: string): T | undefined {
+  const file = `${CONTENT_ROOT}${filename}`
+  if (!existsSync(file)) {
+    return undefined
+  }
+
+  const json = readFileSync(file, 'utf8')
+  if (!json) {
+    return undefined
+  }
+
+  const obj = JSON.parse(json)
+  if (!obj) {
+    return undefined
+  }
+
+  return obj as T
+}
